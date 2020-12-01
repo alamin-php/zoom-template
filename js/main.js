753d55cc79535d207b06eee1b5d5085c07a72741
Md Al Amin Sarker <53163466+alamin-php@users.noreply.github.com>
2020-12-02 00:19:32 +0600

    Image collected
    Image collected and some updated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
     $(document).ready(function() {
         // $('#menu').slicknav();
+        $(".js--services-section").waypoint(function(direction){
+            if(direction == "down"){
+                $("nav").addClass("sticky");
+            }else{
+                $("nav").removeClass("sticky");
+            }
+        });
         var mixer = mixitup('.filter-container');
     });
